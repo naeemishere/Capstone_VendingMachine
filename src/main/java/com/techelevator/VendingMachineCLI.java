200058da0e7a4576972b08834e050d1ecf9120ab
hotspicycook <68454448+hotspicycook@users.noreply.github.com>
2022-12-10 19:03:47 -0600

    I updated the Display vending

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -3,7 +3,9 @@ package com.techelevator;
 import com.techelevator.view.Items;
 import com.techelevator.view.Menu;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class VendingMachineCLI {
@@ -11,6 +13,9 @@ public class VendingMachineCLI {
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
+	//added a exit option
+	private static final String EXIT_OPTION = "Exit";
+
 
 	private Menu menu;
 
@@ -18,20 +23,36 @@ public class VendingMachineCLI {
 		this.menu = menu;
 	}
 
+
+
 	public void run() {
-		Items list = new Items();
-		List <String> vendingList = list.displayItems();
+		restock();
+
+	//Items list = new Items();
+		List <String> vendingList = Items.displayItems();
  		while (true) {
 			String choice = (String) menu.getChoiceFromOptions(MAIN_MENU_OPTIONS);
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
-				for (String str: vendingList) {
-					System.out.println(str);
+				for (Map.Entry<String,Items> str : vendingMachineItems.entrySet()) {
+					System.out.println(str.getKey()+str.getValue());
 				}
 				// display vending machine items
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
 				// do purchase
 			}
+		}
+	}
+	Map<String,Items> vendingMachineItems  = new HashMap<>();
+	//made a map to hold the items after the string[] made.
+	//String itemName, String placement, String typeOfItem
+	public void restock(){
+		//restock method because everytime the program runs we need to restock it to 5
+		for (String str:Items.displayItems()) {
+			String[] eachItem =str.split("\\|");
+
+			Items vendingItem = new Items(Double.parseDouble(eachItem[2]),eachItem[1],eachItem[0],eachItem[3] );
+			vendingMachineItems.put(vendingItem.getPlacement(), vendingItem);
 		}
 	}
 
--- src/main/java/com/techelevator/view/Items.java
+++ src/main/java/com/techelevator/view/Items.java
@@ -2,11 +2,52 @@ package com.techelevator.view;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.text.NumberFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Items {
+    private double price;
+    private String itemName;
+    private String placement;
+    private String typeOfItem;
+    private int amountOfItems=5;
+
+    public int getAmountOfItems() {
+        return amountOfItems;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public String getPlacement() {
+        return placement;
+    }
+
+    public String getTypeOfItem() {
+        return typeOfItem;
+    }
+
+    @Override
+    public String toString() {
+        //String.format("%,.2f", amount));
+        return  "|"+itemName+"|"+ String.format("%.2f",price)+"|"+ typeOfItem;
+
+    }
+
+    public Items(double price, String itemName, String placement, String typeOfItem) {
+        this.price = price;
+        this.itemName = itemName;
+        this.placement = placement;
+        this.typeOfItem = typeOfItem;
+    }
+
     public static List <String> displayItems() {
         String inputFile = "vendingmachine.csv";
         File file = new File(inputFile);
@@ -22,5 +63,6 @@ public class Items {
 //            System.out.println(str);
 //        }
 	return itemLine;
-    }
+
+}
 }
